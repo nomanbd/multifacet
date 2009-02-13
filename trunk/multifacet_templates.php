df819fb553198b30059c811aa326f58ee872f701
rob.casson <rob.casson@42064880-e381-11dd-9059-71fac19e68c6>
2009-02-13 15:51:37 +0000

    (second) initial import

---
+++ trunk/multifacet_templates.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * this file is intended to override
+ * display of records.
+ *
+ * used in conjunction with the *_callback
+ * variables in multifacet.inc.
+ *
+ * examples are in....examples/
+ */
